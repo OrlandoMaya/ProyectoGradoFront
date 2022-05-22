d860eca98becaec4eea393cd9ba6f9ed828a18ce
LotusZaheer <andresfelipeuribe11@gmail.com>
2022-05-21 22:45:38 -0500

    Ahora si funciona de verdad

--- src/app/dashboard/reportes/reportes.component.ts
+++ src/app/dashboard/reportes/reportes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Estacion } from 'src/app/models/estacion.model';
+import {FormControl} from '@angular/forms';
+import { StationService } from 'src/app/services/station.service';
 
 @Component({
   selector: 'app-reportes',
@@ -8,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 
 export class ReportesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _station: StationService) { }
+
+  Station = new FormControl(); // va pa'l hijo
+
+  StationList!: Estacion[];//arreglar despues
 
   ngOnInit(): void {
+    this._station.Get().subscribe(value => {
+      console.log(value.estaciones)
+      this.StationList = value.estaciones;//.map((estacion:Estacion) => estacion.nombre);
+    })
+    this.Station.valueChanges.subscribe(values => {
+    } )
   }
 
 }
