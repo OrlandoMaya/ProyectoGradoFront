a7d30d84c9e853eb1754d83c93ac995586c32d28
OrlandoMR <orlandomoncada610@gmail.com>
2022-05-27 10:42:50 -0500

    Graphics done

--- src/app/dashboard/reportes/hum/hum.component.ts
+++ src/app/dashboard/reportes/hum/hum.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, Input } from "@angular/core";
-
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 import {
   ChartComponent,
@@ -10,8 +15,8 @@ import {
   ApexStroke,
   ApexYAxis,
   ApexTitleSubtitle,
-  ApexLegend
-} from "ng-apexcharts";
+  ApexLegend,
+} from 'ng-apexcharts';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -28,76 +33,74 @@ export type ChartOptions = {
 @Component({
   selector: 'app-hum',
   templateUrl: './hum.component.html',
-  styleUrls: ['./hum.component.scss']
+  styleUrls: ['./hum.component.scss'],
 })
-export class HumComponent implements OnInit {
-
-  @Input()  item!: any[];
-
-  ngOnInit(): void {
-
-    const dataInfo=this.item.map((station) => {
-      return {
-        name: station.station.nombre,
-        data: station.data.map((info: any) => {
-          return info.humedad;
-        }),
-      };
-    })
-    console.log(dataInfo)
-
-    this.chartOptions = {
-      series: dataInfo,
-      chart: {
-        type: "bar",
-        height: 350,
-        zoom: {
-          enabled: false
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        curve: "smooth"
-      },
-
-      title: {
-        text: "Humedad",
-        align: 'center',
-        style: {
-          fontSize:  '18px',
-          fontWeight:  'bold',
-          //color:  'rgba(255, 255, 255, 0.8)'
-        },
-      },
-      labels: 
-      this.item[0].data.map((data:any)=>{return data.fecha}),
-      // [
-      //   "2018-09-19T00:00:00.000Z",
-      //   "2018-09-19T01:30:00.000Z",
-      //   "2018-09-19T02:30:00.000Z",
-      //   "2018-09-19T03:30:00.000Z",
-      //   "2018-09-19T04:30:00.000Z",
-      //   "2018-09-19T05:30:00.000Z",
-      //   "2018-09-19T06:30:00.000Z"
-      // ],
-      xaxis: {
-        type: "datetime"
-      },
-      yaxis: {
-        opposite: true
-      },
-      legend: {
-        horizontalAlign: "left"
-      }
-    };
-  }
+export class HumComponent implements OnInit, OnChanges {
+  @Input() item!: any[];
 
   public chartOptions: Partial<ChartOptions> | any;
 
-  constructor() {
-    
-  }
-}
+  constructor() {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.item.length > 0) {
+      const dataInfo = this.item.map((station) => {
+        return {
+          name: station.station.nombre,
+          data: station.data.map((info: any) => {
+            return info.humedad;
+          }),
+        };
+      });
+      this.chartOptions = {
+        series: dataInfo,
+        chart: {
+          type: 'bar',
+          height: 350,
+          zoom: {
+            enabled: false,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: 'smooth',
+        },
+
+        title: {
+          text: 'Humedad',
+          align: 'center',
+          style: {
+            fontSize: '18px',
+            fontWeight: 'bold',
+            //color:  'rgba(255, 255, 255, 0.8)'
+          },
+        },
+        labels: this.item[0].data.map((data: any) => {
+          return data.fecha;
+        }),
+        // [
+        //   "2018-09-19T00:00:00.000Z",
+        //   "2018-09-19T01:30:00.000Z",
+        //   "2018-09-19T02:30:00.000Z",
+        //   "2018-09-19T03:30:00.000Z",
+        //   "2018-09-19T04:30:00.000Z",
+        //   "2018-09-19T05:30:00.000Z",
+        //   "2018-09-19T06:30:00.000Z"
+        // ],
+        xaxis: {
+          type: 'datetime',
+        },
+        yaxis: {
+          opposite: true,
+        },
+        legend: {
+          horizontalAlign: 'left',
+        },
+      };
+    }
+  }
+
+  ngOnInit(): void {}
+}
--- src/app/dashboard/reportes/maps/maps.component.ts
+++ src/app/dashboard/reportes/maps/maps.component.ts
@@ -1,39 +1,58 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Estacion } from "src/app/models/estacion.model";
-
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { Estacion } from 'src/app/models/estacion.model';
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
-  styleUrls: ['./maps.component.scss']
+  styleUrls: ['./maps.component.scss'],
 })
-export class MapsComponent implements OnInit {
-
-  @Input() item!: Estacion[];
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-
-  }
-
+export class MapsComponent implements OnInit, OnChanges {
+  @Input() item!: any;
   title = 'gmaps';
-  position = {
-    lat: 7.156107,
-    lng: -73.088516
+  // positions = [
+  //   {ubication:{
+  //     lat: 7.156107,
+  //     lng: -73.088516,
+  //   },label:""},
+  // ];
+  positions!:any[];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.getPositions();
   }
 
-  _position(id: string) {
-    for (let i of this.item.map((station:any)=>{return station.station})) {
-      if (i.uid == id) {
-        this.position = {
-          lat: i.latitud,
-          lng: i.longitud,
-        }
-      }
-    }
-    return this.position;
-  }
+  // _position(id: string) {
+  //     for (let i of this.item.map((station:any)=>{return station.station})) {
+  //       if (i.uid == id) {
+  //         this.position = {
+  //           lat: i.latitud,
+  //           lng: i.longitud,
+  //         }
+  //       }
+  //     }
+  //     return this.position;
+  //   }
 
+  getPositions() {
+    this.positions = this.item.map((station: any) => {
+      return {
+        ubication: {
+          lat: station.station.latitud,
+          lng: station.station.longitud,
+        },
+        label: station.station.nombre,
+      };
+    });
+    console.log(this.positions)
+  }
 }
