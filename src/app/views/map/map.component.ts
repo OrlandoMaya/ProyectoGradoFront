64dfc4305cb5405c052a331095aa0de84cae73de
LotusZaheer <andresfelipeuribe11@gmail.com>
2022-06-06 23:16:38 -0500

    Stock

--- src/app/views/map/map.component.ts
+++ src/app/views/map/map.component.ts
@@ -1,33 +1,54 @@
 import { Component, OnInit } from "@angular/core";
 import { Estacion } from "src/app/models/estacion.model";
 import { StationService } from 'src/app/services/station.service';
+import { ControlService } from "src/app/services/control.service";
+import { Control } from "src/app/models/control.model";
+import { ControlContainer, FormControl } from "@angular/forms";
+import { MaterialModule } from '../../material/material.module';
+import { Console } from "console";
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
+
 export class MapComponent implements OnInit {
 
-  StationList!: Estacion[];
+  Station = new FormControl(); // va pa'l hijo
+  StationSelected = [];
+  StationList!: Estacion[]; //arreglar despues
+  data: any[] = [];
+  ControlList!: Control[];
+  Unico!: Control;
 
-  constructor(private _station: StationService) {
+
+  constructor(private _station: StationService, private _control: ControlService) {
   }
 
   ngOnInit(): void {
-    this._station.Get().subscribe(value => {
-      this.StationList = value.estaciones.map((estacion:Estacion) => estacion);
-      console.log(this.StationList)
-    })
+    this._station.Get().subscribe((value) => {
+      this.StationList = value.estaciones.map((estacion: Estacion) => estacion);
+      console.log(this.StationList);
+    });
+
+    this._control.Get().subscribe((resp: any) => {
+      this.ControlList = resp.controles.map((control: Control) => control);
+      //this.ControlList2 = this.ControlList;
+      //this._datos(this.ControlList[this.ControlList.length - 1].idEstacion)
+      //console.log(this._datos(this.ControlList[this.ControlList.length - 1].idEstacion))
+    });
   }
 
   title = 'gmaps';
+
   position = {
     lat: 7.156107,
     lng: -73.088516
   }
 
   _position(id: string) {
+
     for (let i of this.StationList) {
       if (i.uid == id) {
         this.position = {
@@ -38,5 +59,73 @@ export class MapComponent implements OnInit {
     }
     return this.position;
   }
+
+  _datosFec(id: string) {
+    try{for (let i = this.ControlList.length-1; i > 0; i--) {
+      if(this.ControlList[i].idEstacion == id){
+        this.Unico = this.ControlList[i]
+      }
+    }}catch(error){
+    return 0;
+    }
+    return this.Unico.fecha;
+  }
+
+  _datosHumedad(id: string) {
+    try{for (let i = this.ControlList.length-1; i > 0; i--) {
+      if(this.ControlList[i].idEstacion == id){
+        this.Unico = this.ControlList[i]
+      }
+    }}catch(error){
+    return 0;
+    }
+    return this.Unico.humedad;
+  }
+
+  _datosNivCau(id: string) {
+    try{for (let i = this.ControlList.length-1; i > 0; i--) {
+      if(this.ControlList[i].idEstacion == id){
+        this.Unico = this.ControlList[i]
+      }
+    }}catch(error){
+    return 0;
+    }
+    return this.Unico.nivelCauce/100;
+  }
+
+  _datosPrec(id: string) {
+    try{for (let i = this.ControlList.length-1; i > 0; i--) {
+      if(this.ControlList[i].idEstacion == id){
+        this.Unico = this.ControlList[i]
+      }
+    }}catch(error){
+    return 0;
+    }
+    return this.Unico.precipitacion;
+  }
+
+  _datosTemp(id: string) {
+    try{for (let i = this.ControlList.length-1; i > 0; i--) {
+      if(this.ControlList[i].idEstacion == id){
+        this.Unico = this.ControlList[i]
+      }
+    }}catch(error){
+    return 0;
+    }
+    return this.Unico.temperatura;
+  }
+
+  _datosVelCau(id: string) {
+    try{for (let i = this.ControlList.length-1; i > 0; i--) {
+      if(this.ControlList[i].idEstacion == id){
+        this.Unico = this.ControlList[i]
+      }
+    }}catch(error){
+    return 0;
+    }
+    return this.Unico.velocidadCauce;
+  }
+
+
 
 }
